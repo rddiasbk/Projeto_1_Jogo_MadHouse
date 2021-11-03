e6813284bd671da851b7573677841d5137c0ce5e
Roberto Dias <rd_dias@yahoo.com.br>
2021-11-03 13:27:07 -0300

    Acrescentei a descrição dos personagens e uma função para definir a apresentação de cada um deles.

--- JOGO.py
+++ JOGO.py
@@ -1,14 +1,36 @@
-print('_'*30)
+'''print('_'*30)
 print('Veja quem você pode chamar:')
 print('_'*30)
-print('Personagem 1: \nProfissão: piloto: \nHistória:  ')
-print('_'*30)
 
+
+vlad = ('Personagem 1:', 'Vlad Blackwood', 'Profissão:', 'Piloto:','Experiência Paranormal:','Perseguido por Abissais',
+      'Trauma:', 'A incompreensão do que aconteceu \ne as criaturas que viu.')
+
+bill = ('Personagem 2:', 'Bill Liechtenstein', 'Profissão:', 'Agricultor:','Experiência Paranormal:','Viu um Lobisomem',
+      'Trauma:', 'A morte do filho.')
+
+abel = ('Personagem 3:', 'Abel ', 'Profissão:', 'Ladrão:','Experiência Paranormal:','Viu um demônio.',
+      'Trauma:', 'A morte de uma criança.')
+
+def tabela(char):
+    print('#' * 30)
+    for pos in range(0, len(char)):
+        if pos % 2 == 0:
+            print(f'{char[pos]:-<20}', end='')
+        if pos % 2 != 0:
+            print(f' {char[pos]:>5}')
+
+
+tabela(vlad)
+tabela(bill)
+tabela(abel)
+print('_'*30)'''
 
 def opções(opc):
     print('*'*30)
     print(opc)
     print('*'*30)
+
 personagem = ' '
 confirmar = ' '
 porta = ' '
@@ -21,19 +43,26 @@ while confirmar != 's':
          if personagem not in 'vba':
             print('Personagem não encontrado. Tente novamente!')
 
-    #if personagem == 'a':
+if personagem == 'v':
+    print('Olá, eu sou Vlad e estou com pressa, vamos!')
+if personagem == 'a':
+    print('O que vocês querem de novo?! Ah.. Chego em breve!')
+if personagem == 'b':
+    print('...alguma novidade no meu caso?')
 
-while porta not in 'bav':
-    opções('PORTA [1]\nPORTA[2]\nPORTA[3]')
-    porta = int(input('Escolha a porta:'))
-
-    if porta == 1:
-        print('Morreu!')
-    if porta == 2:
-        print('Morreu!')
-    if porta == 3:
-        print('Inimigo')
-
+'''
+while porta not in 'var':
+    opções('PORTA VERDE [V]\nPORTA AMARELA[A]\nPORTA ROXA[R]')
+    porta = str(input('Escolha a porta:')).lower().strip().split()[0]
+    if porta not in 'bar':
+        print('Isso não é uma porta. Tente novamente!')
+if porta == 'v':
+     print('Morreu!')
+if porta == 'a':
+    print('Morreu!')
+if porta == 'r':
+    print('Inimigo')
+'''
 
 
 
