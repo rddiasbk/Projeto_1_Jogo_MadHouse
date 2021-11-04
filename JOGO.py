44c9f45966f2ccd24b26523122cb66f4166d717c
Roberto Dias <rd_dias@yahoo.com.br>
2021-11-04 18:33:14 -0300

    Acrescentei a descrição dos personagens e uma função para definir a apresentação de cada um deles. Acrescentei a etapa de conflito. Quero acrescentar funções para reduzir algumas rotinas. Isto vai ficar pra próxima revisão. Acrescentei trecho de código na sessão main e transformei algumas etapas em função para facilitar a visualização. Agora começa a parte de refinar e começar novas opções.

--- JOGO.py
+++ JOGO.py
@@ -1,71 +1,103 @@
 from random import randint
-print('_'*30)
+
+#formatação de apresentação dos personagens
+def opções(opc):
+    print('*' * 30)
+    print(opc)
+    print('*' * 30)
+
+
+def character():     # ESCOLHA DO PERSONAGEM
+    personagem = ' '
+    confirmar = ' '
+    while confirmar != 's':
+        while personagem not in 'vba':
+            opções('Vlad [V]\nBill [B]\nAbel [A]')
+            personagem = str(input('Escolha uma das opções:')).lower().strip().split()[0]
+            confirmar = str(input('Confirma a escolha? [S/N]?:')).lower().strip().split()[0]
+            if personagem not in 'vba':
+                print('Personagem não encontrado. Tente novamente!')
+    if personagem == 'v':
+        #aprofundar rapidamente o personagem
+        print('Olá, eu sou Vlad e estou com pressa, vamos!')
+    if personagem == 'a':
+        print('O que vocês querem de novo?! Ah.. Chego em breve!')
+    if personagem == 'b':
+        print('...alguma novidade no meu caso?')
+
+
+def main():
+    print(F"{'MAD HOUSE':^40}")
+    print('='*40)
+    print(f'Vocês foram chamados ao Bureau de Pesquisas e Defesa Paranormal (B.P.D.P)')
+
+    comeco = (input('Quando quiser começar, digite a palavra jogar:'))
+    if comeco == 'jogar':
+        character()
+    else:
+        print('Até logo então :)')
+
+
+main()
+
+
+print('_' * 30)
 print('Veja quem você pode chamar:')
-print('_'*30)
-#APRESENTAÇÃO DO PERSONAGEM
-vlad = ('Personagem 1:', 'Vlad Blackwood', 'Profissão:', 'Piloto','Experiência Paranormal:','Perseguido por Abissais',
-      'Trauma:', 'A incompreensão do que aconteceu.')
+print('_' * 30)
+# APRESENTAÇÃO DO PERSONAGEM
+vlad = ('Personagem 1:', 'Vlad Blackwood', 'Profissão:', 'Piloto', 'Experiência Paranormal:', 'Perseguido por Abissais',
+        'Trauma:', 'A incompreensão do que aconteceu.')
+bill = ( 'Personagem 2:', 'Bill Liechtenstein', 'Profissão:', 'Agricultor', 'Experiência Paranormal:', 'Viu um Lobisomem',
+'Trauma:', 'A morte do filho.')
+abel = ('Personagem 3:', 'Abel ', 'Profissão:', 'Ladrão', 'Experiência Paranormal:', 'Viu um demônio.',
+        'Trauma:', 'A morte de uma criança.')
 
-bill = ('Personagem 2:', 'Bill Liechtenstein', 'Profissão:', 'Agricultor','Experiência Paranormal:','Viu um Lobisomem',
-      'Trauma:', 'A morte do filho.')
 
-abel = ('Personagem 3:', 'Abel ', 'Profissão:', 'Ladrão','Experiência Paranormal:','Viu um demônio.',
-      'Trauma:', 'A morte de uma criança.')
-#APRESENTAÇÃO DA TABELA DE PERSONAGENS
+# APRESENTAÇÃO DA TABELA DE PERSONAGENS
 def tabela(char):
     print('#' * 30)
     for pos in range(0, len(char)):
         if pos % 2 == 0:
             print(f'{char[pos]:-<20}', end='')
         if pos % 2 != 0:
-            print(f' {char[pos]:>10}')
-
+            print(f' {char[pos]:>6}')
 
 tabela(vlad)
 tabela(bill)
 tabela(abel)
-print('_'*30)
+
+# Caso o jogador morra
+def jogador_morreu():
+    print('Mais uma vítima da casa!')
+    resposta = (input('Game Over!\n\nDigite J para jogar denovo! :'))
+    if resposta == 'j':
+        main()
+    else:
+        print('Então tá, até a próxima.')
 
 
-def opções(opc):
-    print('*'*30)
-    print(opc)
-    print('*'*30)
-
-
-personagem = ' '
-confirmar = ' '
-porta = ' '
-vitoria = ' '
-#ESCOLHA DO PERSONAGEM
-while confirmar != 's':
-    while personagem not in 'vba':
-        opções('Vlad [V]\nBill [B]\nAbel [A]')
-        personagem = str(input('Escolha uma das opções:')).lower().strip().split()[0]
-        confirmar = str(input('Confirma a escolha? [S/N]?:')).lower().strip().split()[0]
-        if personagem not in 'vba':
-            print('Personagem não encontrado. Tente novamente!')
-if personagem == 'v':
-    print('Olá, eu sou Vlad e estou com pressa, vamos!')
-if personagem == 'a':
-    print('O que vocês querem de novo?! Ah.. Chego em breve!')
-if personagem == 'b':
-    print('...alguma novidade no meu caso?')
+character()
 print('Você chega em uma casa escura e dentro dela se depara com portas diferentes.')
+def doors():
+    porta = ' '
+    while porta not in 'var':
+        opções('PORTA VERDE [V]\nPORTA AMARELA[A]\nPORTA ROXA[R]')
+        porta = str(input('Escolha a porta:')).lower().strip().split()[0]
+        if porta not in 'var':
+            print('Isso não é uma porta. Tente novamente!')
+    if porta == 'v':
+        print('Morreu!')
+    if porta == 'a':
+        print('Morreu!')
+    if porta == 'r':
+        print('Inimigo apareceu!')
+doors()
 
-while porta not in 'var':
-    opções('PORTA VERDE [V]\nPORTA AMARELA[A]\nPORTA ROXA[R]')
-    porta = str(input('Escolha a porta:')).lower().strip().split()[0]
-    if porta not in 'var':
-        print('Isso não é uma porta. Tente novamente!')
-if porta == 'v':
-     print('Morreu!')
-if porta == 'a':
-    print('Morreu!')
-if porta == 'r':
-    print('Inimigo apareceu!')
-    print('Você saca rapidamente e atira:')
+print('Você saca rapidamente e atira:')
 
+
+def combate():
+    vitoria = ' '
     while not vitoria == 0 or vitoria == 1:
         monstro = randint(0, 10)
         ataque_arma = randint(0, 10)
@@ -73,11 +105,11 @@ if porta == 'r':
         print(f'O monstro tinha uma defesa de {monstro}')
         if ataque_arma > monstro:
             print('Você acertou! Ganhou o jogo!')
-            vitoria == 1
+            vitoria = 1
         elif monstro > ataque_arma:
-                    vitoria = 0
-                    print('Você errou!')
-                    print('Agora é a vez do monstro')
+            vitoria = 0
+            print('Você errou!')
+            print('Agora é a vez do monstro')
         monstro = randint(0, 10)
         ataque_arma = randint(0, 10)
         if monstro > ataque_arma:
@@ -85,13 +117,6 @@ if porta == 'r':
             vitoria = 0
             print('Ele te acertou! Você morreu!')
         elif ataque_arma > monstro:
-                vitoria = 1
-                print('Ele errou! Você tem uma nova chance!')
-
-
-
-
-
-
-
-
+            vitoria = 1
+            print('Ele errou! Você tem uma nova chance!')
+combate()
